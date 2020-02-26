0b4aae1be4fb6402f6e44813a489b07dfe06f8a9
Martin Guillon <martin.guillon@akylas.fr>
2020-02-26 17:39:56 +0100

    native CarouselFriendlySwipeRefreshLayout

---
+++ references.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
+/// <reference path="./node_modules/tns-platform-declarations/android-28.d.ts" />
+/// <reference path="./node_modules/nativescript-ui-listview/platforms/ios/typings/listview.d.ts" />
---
+++ src/typings/android.d.ts
@@ -0,0 +1,8 @@
+declare module com {
+  export module nativescript {
+    export module swiperefreshlayout {
+      export class CarouselFriendlySwipeRefreshLayout extends androidx
+        .swiperefreshlayout.widget.SwipeRefreshLayout {}
+    }
+  }
+}
