d5efb45887d349eb1e7d3669306ed0577405cfe7
Martin Guillon <martin.guillon@akylas.fr>
2020-09-30 15:59:54 +0200

    chore: cleanup

--- plugin/pulltorefresh.ios.d.ts
+++
@@ -1,8 +0,0 @@
-import { PullToRefreshBase } from './pulltorefresh-common';
-export * from './pulltorefresh-common';
-export declare class PullToRefresh extends PullToRefreshBase {
-    private _handler;
-    refreshControl: UIRefreshControl;
-    constructor();
-    onLoaded(): void;
-}
